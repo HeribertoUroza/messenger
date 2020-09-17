591ea7af1af9240c40059289cb7623557bf7f13a
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-17 17:21:00 -0400

    successfully added bootstrap and styling

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 function Login() {
     return (
         <>
-            Login
+        <div className='login-container'>
+            <InputGroup className="mb-3 login-form">
+                <FormControl
+                    aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default"
+                    placeholder='Enter Your Name To Login'
+                />
+            </InputGroup>
+        </div>    
         </>
     )
 };
