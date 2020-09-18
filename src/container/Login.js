5fd72979a9f3a1e2f0abf0011d0ef7e49fbbff97
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-18 16:31:13 -0400

    added styling and logo to Login Page

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,19 +1,32 @@
 import React from 'react';
+
+// BOOTSTRAP IMPORTS
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Image from 'react-bootstrap/Image';
+
+// LOGO IMPORT
+import Logo from '../assets/Messenger_Logo.jpg'
 
 function Login() {
     return (
         <>
-        <div className='login-container'>
-            <InputGroup className="mb-3 login-form">
-                <FormControl
-                    aria-label="Default"
-                    aria-describedby="inputGroup-sizing-default"
-                    placeholder='Enter Your Name To Login'
+            <div className='login-container'>
+                <Image 
+                    src={ Logo }
+                    className='login-logo'
+                    rounded 
                 />
-            </InputGroup>
-        </div>    
+                <h1 className='login-title' >Messenger</h1>
+                <InputGroup className="mb-3 login-form">
+                    <FormControl
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                        placeholder='Enter Your Name To Login'
+                        className='login-input'
+                    />
+                </InputGroup>
+            </div>    
         </>
     )
 };
