2881a9a216b7eee8cabcbe41a16ce9903772f407
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-20 19:46:31 -0400

    added firebase. utulizing auth

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// FIREBASE
+import * as firebase from "firebase/app"
+
+// FIREBASE KEYS
+import firebaseKey from './config'
+
+const firebaseConfig = {
+    apiKey: firebaseKey.apiKey,
+    authDomain: firebaseKey.authDomain,
+    databaseURL: firebaseKey.databaseURL,
+    projectId: firebaseKey.projectId,
+    storageBucket: firebaseKey.storageBucket,
+    messagingSenderId: firebaseKey.messagingSenderId,
+    appId: firebaseKey.appId,
+    measurementId: firebaseKey.measurementId
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
