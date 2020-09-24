bf6ec299e9cc1200c22d6be8ec88eef78a85cf9b
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-24 18:59:23 -0400

    was able to implement auth context. currently working

--- src/container/Messages.js
+++ src/container/Messages.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+
+// CONTEXT
+import AuthContext from '../context/AuthContext';
 
 function Messages() {
     return (
         <>
-            Messages
+            <AuthContext.Consumer>
+                {
+                    (isUser) => {
+                        if(isUser){
+                            return (
+                                <>
+                                <div>Hello {isUser}</div>
+                                </>
+                            )
+                        } else {
+                            return <Redirect to='/' />
+                        }
+                    }
+                }
+            </AuthContext.Consumer>
         </>
     )
 }
