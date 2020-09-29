30f3852e8b0c3798b91480084cfafa6a2dda0d9b
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-29 12:22:07 -0400

    created mock data to test. create messages component to reuse

---
+++ src/component/SingleMessage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function SingleMessage(props) {
+    console.log(props)
+    return (
+        <>
+            messagesss
+        </>
+    )
+}
+
+export default SingleMessage;
+/*
+
+
+Aenean orci mi, consectetur imperdiet risus at, efficitur eleifend justo. Etiam pellentesque felis nec vulputate pellentesque. Phasellus interdum condimentum nulla, id facilisis nisl viverra sed. Suspendisse id auctor erat. Sed blandit lectus quis lacus dictum, vel fermentum nibh porttitor. Etiam vel felis non lorem cursus sollicitudin. Ut sodales nisi non ligula vestibulum, id dictum leo rhoncus. Etiam auctor, sapien ac facilisis iaculis, ex magna commodo arcu, id tempor libero ante facilisis dui.
+
+Maecenas sit amet eros eu mi molestie euismod sed vitae arcu. Sed posuere varius quam, sed bibendum sapien hendrerit quis. Curabitur ligula quam, ornare nec porta ut, fringilla ut libero. Praesent eget hendrerit tortor. Aenean at volutpat nulla. Nullam tempus rutrum odio, id luctus purus sagittis ac. Praesent rutrum porttitor euismod. In a vehicula arcu. Duis sed volutpat risus. Nunc eu mollis odio. Donec eu sollicitudin turpis, non ultrices mauris. Sed vel dui magna.
+
+Proin luctus id ligula sed maximus. Nulla efficitur risus non convallis congue. Sed eu tristique risus, non convallis urna. Quisque euismod ut dolor ut dapibus. Aliquam pulvinar est ultricies sem sodales, eu pellentesque odio feugiat. Mauris egestas, felis a eleifend tristique, metus eros dignissim quam, quis ullamcorper dui ex eget lectus. Mauris egestas mauris eget diam dictum, et hendrerit arcu vestibulum. In hac habitasse platea dictumst. Duis eget ullamcorper urna, in condimentum nisi. Nulla dolor nunc, porta sed leo sit amet, blandit imperdiet erat. Nunc vulputate accumsan lorem quis fermentum. Duis pharetra lacus quis mi hendrerit consectetur.
+
+Mauris sed cursus lectus. Aenean id accumsan quam. In quis lorem fermentum, scelerisque tortor at, lacinia nulla. Suspendisse potenti. Vestibulum blandit justo id aliquet viverra. Donec odio elit, hendrerit a sapien eu, efficitur fermentum ante. Donec viverra felis ipsum.
+
+
+
+
+
+
+
+Etiam dignissim auctor aliquet. Aliquam vitae mauris pulvinar, bibendum felis at, varius metus. Nam maximus nisi at massa viverra tristique. Quisque eu odio non lacus consequat bibendum at at elit. Donec consectetur, libero vel eleifend vehicula, mi nulla commodo risus, eget feugiat quam purus eu erat. Nulla hendrerit augue cursus placerat volutpat. Proin eget metus enim. Integer placerat at massa id iaculis. Aenean sollicitudin nisl ut bibendum pretium. Aliquam facilisis ligula nec nisl ultricies, at facilisis tellus tempus. Ut id tortor orci. Integer elementum leo quam, at iaculis dolor rutrum congue. Pellentesque ac orci nec massa accumsan laoreet nec id metus. Mauris dapibus vestibulum risus quis consectetur.
+*/
