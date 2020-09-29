22d57f0d5808ee4495657f5a93ed18024451f8ac
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-29 14:47:55 -0400

    added styling

--- src/component/SingleMessage.js
+++ src/component/SingleMessage.js
@@ -17,6 +17,7 @@ function SingleMessage(props) {
     return (
         <>
             <div className='s_Message_con' key={props.data_key} >
+                <label className='s_Message_label' style={handleStyle(props.fromMe)}>{props.email}</label>
                 <div className='s_Message' style={handleStyle(props.fromMe)} >{props.message}</div>
             </div>
         </>
