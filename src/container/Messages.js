980454889cab08d63106798d758881017649359d
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-29 19:04:50 -0400

    function working, need to add socket io

--- src/container/Messages.js
+++ src/container/Messages.js
@@ -14,29 +14,28 @@ import Button from 'react-bootstrap/Button';
 
 function Messages() {
     const [ textInput, getTextInput ] = useState('');
-
-    const data = [
+    const [data, getData] = useState([
         {
             email: 'person 1',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et felis id leo scelerisque laoreet. Maecenas vitae nulla luctus erat placerat scelerisque non eget dui. Donec ipsum libero, mollis non neque vitae, accumsan commodo felis. Ut ac suscipit velit. Proin eu laoreet tortor. Pellentesque condimentum diam vel felis facilisis, interdum porta nisi dictum. Vivamus venenatis, tortor eu fermentum aliquet, nisl diam iaculis elit, ac bibendum nisi magna vitae nunc. Fusce at tincidunt tellus. Fusce accumsan scelerisque nulla ac laoreet.',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et felis id leo scelerisque laoreet. Maecenas vitae nulla luctus erat et.',
             fromMe: true
         },
         {
             email: 'person 2',
-            message: 'Ut semper, leo id auctor tincidunt, risus sem scelerisque nisl, quis porta purus velit sed ante. Cras maximus lorem vel eleifend tincidunt. Aliquam facilisis aliquet purus ut ultricies. Morbi ac ante in enim suscipit scelerisque eget a dui. Vestibulum efficitur placerat urna. Etiam laoreet euismod nunc, ac aliquet tellus feugiat at. Nullam vitae eros nec tellus suscipit iaculis non eget est. Donec dictum vel tortor et gravida. Aliquam at sapien in eros placerat aliquam. Nunc fermentum lectus ac dui luctus malesuada. Ut ultrices, velit sed dignissim imperdiet, felis odio convallis augue, a tincidunt felis velit sed urna. In vestibulum, nunc in volutpat suscipit, nulla tellus condimentum nisi, ut molestie ipsum nibh in sapien. In eu mauris quis arcu pulvinar gravida. In et pellentesque mauris, sit amet scelerisque nibh.',
+            message: 'Ut semper, leo id auctor tincidunt, risuscies. Morbi ac ante in enim suscipit scelerisque eget a dui. Vestibulum efficitur placerat urna. Etiam laoreet euismod nunc, ac aliquet tellus feugiat at. Nullam vitae eros nec tellus suscipit iaculis non eget est. Donec dictum vel tortor et gravida. Aliquam at sapien in eros placerat aliquam. Nunc fermentum lectus ac dui luctus malesuada. Ut ultrices, velit sed dignissi velit sed urna. In vestibulum, nunc in volutpat suscipit, nulla tellus condimentum nisi, ut molestie ipsum nibh in sapien. In eu mauris quis arcu pulvinar gravida.',
             fromMe: false
         },
         {
             email: 'person 1',
-            message: 'Integer id viverra nisl, non molestie lacus. Nunc a accumsan odio. Etiam mattis lectus in sagittis tristique. Integer sit amet quam in risus venenatis tincidunt. Cras eget tellus ac ligula lacinia accumsan. Vestibulum ullamcorper vestibulum consectetur. Nullam venenatis ipsum et sodales facilisis. Duis congue sollicitudin turpis, quis efficitur purus scelerisque quis. Maecenas a purus sed augue accumsan vestibulum. Aliquam erat volutpat. Morbi eu augue a ligula aliquam porta vel eget massa. Nulla sit amet condimentum magna. Aenean quis erat libero.',
+            message: 'Integer id viverra nisl, non molestie lacus. uis erat libero.',
             fromMe: true
         },
         {
             email: 'person 3',
-            message: 'Ut at semper quam. Morbi dolor mauris, ultricies tempus hendrerit nec, dignissim sed sapien. Ut nisl nisl, aliquet at mauris eget, tristique porta arcu. In nec magna ornare, fringilla lectus a, tincidunt leo. Vivamus gravida placerat enim, et fermentum sem. Morbi bibendum posuere nunc a lacinia. Morbi finibus libero ac egestas feugiat. Integer ultricies consectetur tellus ac ullamcorper. Aenean ornare justo metus, id dictum purus imperdiet tincidunt. Proin non neque placerat, rutrum turpis eget, luctus tellus. Integer aliquet nisi neque, vitae finibus mauris malesuada eu.Nam condimentum ipsum quis accumsan malesuada.Ut iaculis ex id sem vehicula fringilla.Duis sagittis enim et nisi congue sollicitudin.Sed auctor metus augue, et efficitur felis mollis eget.Proin et libero quis felis venenatis molestie vel nec nisl.Curabitur lorem ex, lobortis at justo in, porta auctor ipsum.Praesent feugiat rutrum augue, eu commodo leo tempus a.Nulla a nisl lacus.Sed id interdum mi.Cras molestie ipsum tellus, non laoreet urna lobortis porta.Sed interdum ultricies justo, in porta nisl eleifend id.Aenean vitae bibendum magna, quis imperdiet nisl.Morbi et ex convallis, molestie tortor quis, sagittis tellus.Praesent est sapien, iaculis nec mauris ut, lobortis rhoncus magna.Sed et neque dignissim, blandit nunc a, feugiat velit.',
+            message: 'Ut at sempe a nisl lalit.',
             fromMe: false
         },
-    ];
+    ])
 
     const user = useContext(AuthContext)
 
@@ -44,13 +43,28 @@ function Messages() {
         getTextInput(e.target.value)
     };
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        let newData = [...data]
+
+        newData.push({
+            email: user,
+            message: textInput,
+            fromMe: true
+        })
+        getData(newData);
+        getTextInput('');
+        let message_text_input = document.querySelector('.message-text-input');
+        message_text_input.value = '';
+    }
+
 
     return (
         <>
             <AuthContext.Consumer>
                 {
                     (isUser) => {
-                        if(!isUser){
+                        if(isUser){
                             return (
                                 <>
                                     <div className='messages-container'>
@@ -75,9 +89,10 @@ function Messages() {
                                                     aria-label="messageText"
                                                     aria-describedby="basic-addon2"
                                                     onChange={handleOnChange}
+                                                    className='message-text-input'
                                                 />
                                                 <InputGroup.Append>
-                                                    <Button variant="outline-secondary">Send</Button>
+                                                    <Button variant="outline-secondary" onClick={handleSubmit}>Send</Button>
                                                 </InputGroup.Append>
                                             </InputGroup>
                                         </div>
