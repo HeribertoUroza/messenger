b8ba4acdd7a6796b432b3f8297ac30a76743b077
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-30 15:24:24 -0400

    found error in styling, used flex to correctly align lable

--- src/component/SingleMessage.js
+++ src/component/SingleMessage.js
@@ -2,25 +2,50 @@ import React from 'react';
 
 function SingleMessage(props) {
     
-    const handleStyle = ( fromMe ) => {
+    const handleMessageStyle = ( fromMe ) => {
         if(fromMe) {
             return {
                 marginLeft: '20%',
-                boxShadow: '6px 4px 17px 2px rgba(0,0,255,0.33)'
+                boxShadow: '6px 4px 17px 2px rgba(0, 116, 253,0.33)'
             }
         } else {
             return {
                 marginRight: '20%',
-                boxShadow: '-6px 4px 17px 2px rgba(0,0,255,0.33)'
+                boxShadow: '-6px 4px 17px 2px rgba(0, 116, 253,0.33)'
             }
         }
     }
 
+    const handleLableStyle = ( fromMe ) => {
+        if(fromMe) {
+            return {
+                marginLeft: '65%',
+                boxShadow: '6px 4px 17px 2px rgba(0, 116, 253,0.33)'
+            } 
+        } else {
+            return {
+                marginRight: '20%',
+                boxShadow: '-6px 4px 17px 2px rgba(0, 116, 253,0.33)'
+            }
+        }
+    }
+
+    const handleLableContainer = ( fromMe ) => {
+        if(fromMe) {
+            return {
+                display: 'flex',
+                flexDirection: 'row-reverse'
+            }
+        }
+    };
+
     return (
         <>
             <div className='s_Message_con' key={props.data_key} >
-                <label className='s_Message_label' style={handleStyle(props.fromMe)}>{props.email}</label>
-                <div className='s_Message' style={handleStyle(props.fromMe)} >{props.message}</div>
+                    <div className='s_l_container' style={handleLableContainer(props.fromMe)}>
+                        <label className='s_Message_label' style={handleLableStyle(props.fromMe)}>{props.email}</label>
+                    </div>
+                <div className='s_Message' style={handleMessageStyle(props.fromMe)} >{props.message}</div>
             </div>
         </>
     )
