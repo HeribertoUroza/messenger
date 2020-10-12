4496098e5f1362bb18a4d845737392f2a4604f0c
Heriberto Uroza <heribertouroza@pursuit.org>
2020-10-12 13:40:01 -0400

    added env variable for netlify

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,17 +3,17 @@ import * as firebase from "firebase/app";
 import 'firebase/auth';
 
 // FIREBASE KEYS
-import firebaseKey from './config'
+//import firebaseKey from './config'
 
 const firebaseConfig = {
-    apiKey: firebaseKey.apiKey,
-    authDomain: firebaseKey.authDomain,
-    databaseURL: firebaseKey.databaseURL,
-    projectId: firebaseKey.projectId,
-    storageBucket: firebaseKey.storageBucket,
-    messagingSenderId: firebaseKey.messagingSenderId,
-    appId: firebaseKey.appId,
-    measurementId: firebaseKey.measurementId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
